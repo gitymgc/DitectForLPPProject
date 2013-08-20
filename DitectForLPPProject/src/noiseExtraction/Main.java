00578e711dfdaace8ca2a8549bfe715dd5903de3
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-20 16:10:25 +0900

    noiseExtractionのMainクラス作成

---
+++ DitectForLPPProject/src/noiseExtraction/Main.java
@@ -0,0 +1,16 @@
+package noiseExtraction;
+
+public class Main {
+	
+	public static void main(String[] args)throws Exception{
+		
+		new Main().exec();
+		
+	}
+	
+	public void exec()throws Exception{
+		
+	}
+	
+
+}
