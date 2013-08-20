81440399485a1da289193417322a10353e47dbae
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-20 18:22:25 +0900

    Parameterクラス作成

---
+++ DitectForLPPProject/src/characterCandidateDitectionProcess/Parameter.java
@@ -0,0 +1,22 @@
+package characterCandidateDitectionProcess;
+
+public class Parameter {
+	
+	public Parameter(){
+		
+		int maxNum ;
+		int minNum ;
+		int expNum ;
+
+		String srcDirPath;
+		String dstDirPath;
+
+		int h;
+		int w;
+		int mSize;
+		int mh;
+		int mhForBin;
+		
+	}
+
+}
