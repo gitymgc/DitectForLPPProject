d8d70af63aaaf26c1c8d4e83e44c215862d5ec8e
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-20 19:20:56 +0900

    Parameter設定

--- DitectForLPPProject/src/characterCandidateDitectionProcess/Parameter.java
+++ DitectForLPPProject/src/characterCandidateDitectionProcess/Parameter.java
@@ -1,22 +1,46 @@
 package characterCandidateDitectionProcess;
 
 public class Parameter {
-	
-	public Parameter(){
-		
-		int maxNum ;
-		int minNum ;
-		int expNum ;
 
-		String srcDirPath;
-		String dstDirPath;
+	//メンバ変数で定義
+	//共用
+	public int mSize;
+	public int mh;
+	public int mhForBin;
 
-		int h;
-		int w;
-		int mSize;
-		int mh;
-		int mhForBin;
-		
+	//BaseImageCreation用
+	public int epsilon;
+	public int smthNum;
+
+	//NioseExtraction用
+	public int maxNum;
+	public int minNum;
+	public int expNum;
+
+
+	/**
+	 * パラメータ
+	 * @param mSize
+	 * @param mhForBin
+	 * @param epsilon
+	 * @param smthNum
+	 * @param maxNum
+	 * @param minNum
+	 * @param expNum
+	 */
+	public Parameter(int mSize,int mhForBin,int epsilon, int smthNum,int maxNum,int minNum,int expNum){
+		//コンクラスタで値を格納
+		this.mSize = mSize;
+		this.mh = mSize/2;
+		this.mhForBin = mhForBin;
+
+		this.epsilon = epsilon;
+		this.smthNum = smthNum;
+
+		this.maxNum = maxNum;
+		this.minNum = minNum;
+		this.expNum = expNum;
+
 	}
 
 }
