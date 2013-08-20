6e051c674606cd8d49f493a569b0f56ed67bf1cd
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-20 12:04:39 +0900

    EpsilonFilter完成、汎用化
    TestEpsilonFilterから名称変更
    汎用化

---
+++ DitectForLPPProject/src/main/EpsilonFilter.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.awt.image.BufferedImage;
+
+public class EpsilonFilter {
+
+	public static void exec(BufferedImage srcImg,int gra2d[][] , int mSize,int e,int N) throws Exception{
+
+		int w = srcImg.getWidth();
+		int h = srcImg.getHeight();
+
+		//局所処理準備
+		int mh = mSize/2;
+		int q = ((int)Math.pow((2*mh +1),2))-1;
+		int dst2d[][] = new int[h][w];
+
+		//平滑化
+		for(int i = 0; i < N; i++){
+			for(int y = mh; y <h-mh; y++){
+				for(int x = mh; x < w-mh; x++){
+
+					int tmp = 0;
+
+					for(int my = -mh; my <= mh; my++){
+						for(int mx = -mh; mx <= mh; mx++){
+							
+							//真ん中飛ばす
+							if(my == 0 && mx == 0)continue;
+							if(Math.abs(gra2d[y][x] - gra2d[y+my][x+mx]) <= e){
+								tmp += gra2d[y+my][x+mx];
+							}else{
+								tmp += gra2d[y][x];
+							}
+						}
+					}
+					dst2d[y][x] = (int)((double)tmp/q);
+					gra2d[y][x] = dst2d[y][x];
+				}
+			}
+		}
+	}
+}
