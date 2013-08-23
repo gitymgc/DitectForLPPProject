908f4bc124ed17665ac9f75a5fad3521d26d70a0
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-23 11:02:33 +0900

    文字抽出、ベクトル正規化まで

--- DitectForLPPProject/src/characterExtractionProcess/Main.java
+++ DitectForLPPProject/src/characterExtractionProcess/Main.java
@@ -47,13 +47,13 @@ public class Main {
 			for(int y = 0; y < h; y++){
 				for(int x = 0; x <w; x++){
 					if((src2d[y][x] -50) > 0){
-					lowGra2d[y][x] = (int)(src2d[y][x] - 50);
+						lowGra2d[y][x] = (int)(src2d[y][x] - 50);
 					}else{
 						lowGra2d[y][x] = 0;
 					}
 				}
 			}
-			
+
 			//反転
 			int opt2d[][] = new int[h][w];
 			for(int y = 0; y < h; y++){
@@ -63,15 +63,80 @@ public class Main {
 			}
 
 			int binN2d[][] = new int[h][w];
-//			NoiseExtractionOpt.exec(param, srcImg, src2d, binN2d);
-//			NoiseExtraction.exec(param, srcImg, opt2d, binN2d);
+			//			NoiseExtractionOpt.exec(param, srcImg, src2d, binN2d);
+			//			NoiseExtraction.exec(param, srcImg, opt2d, binN2d);
 			NoiseExtraction.exec(param, srcImg, lowGra2d, binN2d);
 
+			//文字候補抽出
 			int neoBin2d[][] = new int[h][w];
 			CharacterCandidateDitection.exec(param, srcImg, binB2d, binN2d, neoBin2d);
-			
-			
-			
+
+			//文字抽出
+			//重心移動
+			//コピー
+			int binW2d[][] = new int[h][w];
+			for(int y = 0; y < h; y++){
+				for(int x = 0; x < w; x++){
+					binW2d[y][x] = neoBin2d[y][x];
+				}
+			}
+			//局所重心、移動ベクトル算出
+			int vect[][][] = new int[h][w][2];
+			for(int y = param.mh; y < h-param.mh; y++){
+				for(int x = param.mh; x < w-param.mh; x++){
+					int r = 0;
+					int s = 0;
+					for(int my = -param.mh; my < param.mh; my++){
+						for(int mx = -param.mh; mx < param.mh; mx++){
+							if(my == 0 && mx == 0)continue;
+							r += mx * binW2d[y+my][x+mx];
+							s += my * binW2d[y+my][x+mx];
+						}
+					}
+					vect[y][x][0] = r;
+					vect[y][x][1] = s;
+				}
+			}
+			//ベクトル正規化
+			int theta[][][]= new int[h][w][2];
+			for(int y = 0; y < h; y++){
+				for(int x = 0; x < w; x++){
+					int r = vect[y][x][0];
+					int s = vect[y][x][1];
+					int absR = Math.abs(r);
+					int absS = Math.abs(s);
+					if(r > 0 && 2*absS < absR){
+						theta[y][x][0] = 1;
+						theta[y][x][1] = 0;
+					}else if( r > 0 && s > 0 && absR/2 <= absS && absS <= 2*absR){
+						theta[y][x][0] = 1;
+						theta[y][x][1] = 1;
+					}else if(r > 0 && s < 0 && absR/2 <= absS && absS <= absR){
+						theta[y][x][0] = 1;
+						theta[y][x][1] = -1;
+					}else if(r < 0 && 2*absS < absR){
+						theta[y][x][0] = -1;
+						theta[y][x][1] = 0;
+					}else if( r < 0 && s > 0 && absR/2 <= absS && absS <= 2*absR){
+						theta[y][x][0] = -1;
+						theta[y][x][1] = 1;
+					}else if( r < 0 && s < 0 && absR/2 <= absS && absS <= 2*absR){
+						theta[y][x][0] = -1;
+						theta[y][x][1] = -1;
+					}else if (s > 0 && 2*absR < absS){
+						theta[y][x][0] = 0;
+						theta[y][x][1] = 1;
+					}else if(s < 0 && 2*absR <= absS){
+						theta[y][x][0] = 0;
+						theta[y][x][1] = -1;
+					}else{
+						theta[y][x][0] = 0;
+						theta[y][x][1] = 0;
+					}
+				}
+			}
+
+
 			String dstFilePath = dstDirPath + srcFile.getName();
 			String dstElem[] = srcFile.getName().split("\\.");
 			File dstFile = new File(dstFilePath);
@@ -81,11 +146,11 @@ public class Main {
 
 			for(int y = 0; y < h; y++){
 				for(int x = 0; x < w; x++){
-//					dstBuf.setElem(y*w+x, lowGra2d[y][x]);
+					//					dstBuf.setElem(y*w+x, lowGra2d[y][x]);
 					dstBuf.setElem(y*w+x, (neoBin2d[y][x] != 0)?255:0);
-//										dstBuf.setElem(y*w+x, (binDef2d[y][x] != 0)?255:0);
-//										dstBuf.setElem(y*w+x, (binN2d[y][x] != 0)?255:0);
-//										dstBuf.setElem(y*w+x, (binB2d[y][x] != 0)?255:0);
+					//										dstBuf.setElem(y*w+x, (binDef2d[y][x] != 0)?255:0);
+					//										dstBuf.setElem(y*w+x, (binN2d[y][x] != 0)?255:0);
+					//										dstBuf.setElem(y*w+x, (binB2d[y][x] != 0)?255:0);
 				}
 			}
 			ImageIO.write(dstImg, "bmp", dstFile);
